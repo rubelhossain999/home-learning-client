adbd6a3756321de9f3bda9900ee24d91262e3971
Rubel Hossain99 <rubel820746@gmail.com>
2022-10-24 22:31:41 +0600

    Fontawsome install is done with config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Developer</h1>
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
