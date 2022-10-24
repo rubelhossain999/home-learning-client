0b67fe5aa96030557b3562a7dda9180bc688f65a
Rubel Hossain99 <rubel820746@gmail.com>
2022-10-24 23:04:15 +0600

    Google firebase with config install is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Developer</h1>
       <FontAwesomeIcon icon={faCoffee} />
+      <Toaster></Toaster>
     </div>
   );
 }
